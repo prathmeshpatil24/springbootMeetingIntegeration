ac5165978ad6ad7359846db8a76de08a6bacac1f
prathmeshpatil24 <patilprathmesh365@gmail.com>
2025-11-19 18:53:21 +0530

    project setup

---
+++ src/main/java/com/example/ZoomMeeting/ZoomMeetingApplication.java
@@ -0,0 +1,15 @@
+package com.example.ZoomMeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomMeetingApplication.class, args);
+
+        System.out.println("Zoom Meeting Application Started Successfully!");
+	}
+
+}
---
+++ src/test/java/com/example/ZoomMeeting/ZoomMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZoomMeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
